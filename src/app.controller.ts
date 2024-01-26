2b2be8391682fb3deb0b607a99db658df2460da7
Tran Thanh Tung <tung.webdeveloper@gmail.com>
2024-01-26 19:30:05 +0700

    Feat: I18n config.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Hello API!';
+  async getHello(@I18n() i18n: I18nContext): Promise<string> {
+    return await i18n.t('message.hello');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import {
+  AcceptLanguageResolver,
+  I18nModule,
+  QueryResolver,
+  HeaderResolver,
+} from 'nestjs-i18n';
 
 import config from '@config/configApp';
 import { AppController } from './app.controller';
@@ -11,6 +18,18 @@ import { AppController } from './app.controller';
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
     }),
   ],
   controllers: [AppController],
