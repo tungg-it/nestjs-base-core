2d7ff73369e8251c4c250ee6586d945740f7ccfa
Tran Thanh Tung <tung.webdeveloper@gmail.com>
2025-10-19 17:03:14 +0700

    Fix: Add option expose port for service.

--- libs/core/src/app/boot.ts
+++ libs/core/src/app/boot.ts
@@ -11,12 +11,13 @@ import { ResponseInterceptor } from './response';
 
 export interface AppOptions {
   appName: string;
+  exposePort?: boolean;
 }
 export const startApp = async (
   AppModule: Type<unknown>,
   options: AppOptions,
 ) => {
-  const { appName } = options;
+  const { appName, exposePort } = options;
   const logger = new Logger(appName);
 
   const app = await NestFactory.create(AppModule);
@@ -26,53 +27,65 @@ export const startApp = async (
   const apiDocument = config.get<string>('apiDocument');
   const prefix = `${appName === 'api' ? appName : appName + '/api'}/v1`;
 
-  app.setGlobalPrefix(prefix);
-  app.enableCors();
-  app.enableVersioning();
+  if (exposePort) {
+    app.setGlobalPrefix(prefix);
+    app.enableCors();
+    app.enableVersioning();
 
-  app.useGlobalPipes(new I18nValidationPipe());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      stopAtFirstError: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+    app.useGlobalPipes(new I18nValidationPipe());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        stopAtFirstError: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
 
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalInterceptors(new TimeoutInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter(config));
+    app.useGlobalInterceptors(new ResponseInterceptor());
+    app.useGlobalInterceptors(new TimeoutInterceptor());
+    app.useGlobalFilters(new HttpExceptionFilter(config));
 
-  // Config for development
-  if (environment === 'development') {
-    // Morgan config
-    app.use(morganMiddleware);
+    // Config for development
+    if (environment === 'development') {
+      // Morgan config
+      app.use(morganMiddleware);
 
-    // Swagger config
-    const docOptions = {
-      swaggerOptions: {
-        persistAuthorization: true,
-        displayRequestDuration: true,
-        docExpansion: 'none',
-        filter: true,
-        showRequestHeaders: true,
-      },
-    };
+      // Swagger config
+      const docOptions = {
+        swaggerOptions: {
+          persistAuthorization: true,
+          displayRequestDuration: true,
+          docExpansion: 'none',
+          filter: true,
+          showRequestHeaders: true,
+        },
+      };
 
-    const config = new DocumentBuilder()
-      .setTitle('API example')
-      .setDescription('API for development.')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(`${prefix}/${apiDocument}`, app, document, docOptions);
+      const config = new DocumentBuilder()
+        .setTitle('API example')
+        .setDescription('API for development.')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup(
+        `${prefix}/${apiDocument}`,
+        app,
+        document,
+        docOptions,
+      );
+    }
+
+    await app.listen(port, '0.0.0.0');
+
+    logger.log(
+      `Service ${appName} HTTP is running on http://localhost:${port}/${prefix}`,
+    );
+    logger.log(`Environment: ${environment}`);
+  } else {
+    await app.init();
+    logger.log(`Service ${appName} started successfully`);
+    logger.log(`Environment: ${environment}`);
   }
-
-  await app.listen(port, '0.0.0.0');
-
-  logger.log(
-    `App ${appName} HTTP server is running on http://localhost:${port}/${prefix}`,
-  );
 };
