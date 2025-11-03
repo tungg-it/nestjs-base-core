c2b13aa577ccd14772150c8e2c276f6f9981126b
Tran Thanh Tung <tung.webdeveloper@gmail.com>
2025-11-03 15:57:52 +0700

    Fix: Version and CORS

--- libs/core/src/app/boot.ts
+++ libs/core/src/app/boot.ts
@@ -1,5 +1,5 @@
 import 'source-map-support/register';
-import { Logger, Type, ValidationPipe } from '@nestjs/common';
+import { Logger, Type, ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
@@ -25,12 +25,24 @@ export const startApp = async (
   const port = config.get<number>(appName.toLowerCase() + 'Port');
   const environment = config.get<string>('environment');
   const apiDocument = config.get<string>('apiDocument');
-  const prefix = `${appName === 'api' ? appName : appName + '/api'}/v1`;
+  const prefix = `${appName === 'api' ? appName : appName + '/api'}`;
 
   if (exposePort) {
     app.setGlobalPrefix(prefix);
-    app.enableCors();
-    app.enableVersioning();
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: '1',
+    });
+
+    app.enableCors({
+      origin: ['*'], // Allows many domain
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Language'],
+      exposedHeaders: ['Content-Disposition'], // If return file download
+      credentials: true, // Allows cookie, Authorization header
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    });
 
     app.useGlobalPipes(new I18nValidationPipe());
     app.useGlobalPipes(
@@ -81,6 +93,9 @@ export const startApp = async (
 
     logger.log(
       `Service ${appName} HTTP is running on http://localhost:${port}/${prefix}`,
+    );
+    logger.log(
+      `API Document service ${appName}: http://localhost:${port}/${prefix}/${apiDocument}`,
     );
     logger.log(`Environment: ${environment}`);
   } else {
